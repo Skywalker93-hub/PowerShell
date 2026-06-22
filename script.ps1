226396502ab60711da8064838d61ca2480794342
Skywalker93-hub <nothern4lights@gmail.com>
2026-06-22 14:58:41 +0300

    Improved the script: added a modification that could calculate an average response time for ports 80 and 443

--- script.ps1
+++ script.ps1
@@ -1,67 +1,100 @@
 Param(
-    [string]$website, 
-	[string]$today = (Get-Date -Format 'dd-MM-yyyy HH:mm:ss'),
-    [array]$urls = @(
+    [string]$website
+)
+
+# List of variables
+
+$today = (Get-Date -Format 'dd-MM-yyyy HH:mm:ss')
+$urls = @(
 		"http://$website", 
-		"https://$website"
+		"https://$website")
+$logfile = "./script.log"
+$time = @()
+
+Function Caltime {
+
+    # Count the average response time for ports 80 and 443, and write the output to the log.txt file
+
+	Param(
+		[array]$time
 	)
 
-)
+	if ( !$time ) {
+		write-output 'Parameter is empty. The average response time for ports 80 and 443 cannot be calculated'
+		add-content -path $logfile -value "${today}: Parameter is empty. The average response time for ports 80 and 443 cannot be calculated"
+		exit 1
+	}
+	$avg_time = ($time / 6)
+	write-output "${today}: The average response time for ports 80 and 443 is: $avg_time milliseconds"
+	add-content -path $logfile -value "${today}: The average response time for ports 80 and 443 is: $avg_time milliseconds"
+
+}
 
 Function WebRequest {
 
- # Stop the script if the parameter isn't provided, and write the output to the log.txt file
+# Stop the script if the parameter isn't provided, and write the output to the log.txt file
 
     if ( !$website ) {
-        echo 'Not correct. Add this part after the name of the script: "... mail.ru'
-        add-content -path "/var/log/web_checks/script.log" -value "${today}: a website's URL isn't added"
+        write-output 'Not correct. Add this part after the name of the script: "... mail.ru'
+        add-content -path $logfile -value "${today}: a website's URL wasn't added"
         exit 1
     }
-
- # Check the availability of $website and and write the output to the log.txt file
+ 
+ # Check the availability of website and and write the output to the log.txt file
 	
 	Try {
-    	$response = iwr -uri $website -TimeoutSec 10 -ErrorAction Stop
-		echo "The request is True"
-		add-content -path "/var/log/web_checks/script.log" -value "${today}: the request is True"
+    	$response = invoke-webrequest -uri $website -TimeoutSec 10 -ErrorAction Stop
+		write-output "The request is True"
+		add-content -path $logfile -value "${today}: the request is True"
 			
 		if ($response.StatusCode -eq 200) {
-			add-content -path "/var/log/web_checks/script.log" -value "${today}: the status code of the website is: $($response.StatusCode)"
-			echo "${today}: The status code of the website is: $($response.StatusCode)"
+			add-content -path $logfile -value "${today}: the status code of the website is: $($response.StatusCode)"
+			write-output "${today}: The status code of the website is: $($response.StatusCode)"
 		} else {
-			add-content -path "/var/log/web_checks/script.log" -value "${today}: the website is not available: $($response.StatusCode)"
-			echo "${today}: The website is not available: $($response.StatusCode)"
+			add-content -path $logfile -value "${today}: the website is not available: $($response.StatusCode)"
+			write-output "${today}: The website is not available: $($response.StatusCode)"
 		exit 1
 		}
 	}
 
 	Catch {
-    	echo "The request is False"
-    	add-content -path "/var/log/web_checks/script.log" -value "${today}: the request is False"
+    	write-output "The request is False"
+    	add-content -path $logfile -value "${today}: the request is False"
 		exit 1
 	}
+
+ # Create a stopwatch to measure the execution time of checking the ports availability  
+
 
  # Check the availability three times for ports 80 and 443, and and write the output to the log.txt file
 
 	foreach ($x in $urls) {
 
-    for ($a = 1; $a -le 3; $a++) {
-		$response2 = iwr $x
-        if ($response2.StatusCode -eq 200) {
-            echo "${today}: $x StatusCode: $($response2.StatusCode) and StatusDescription: $($response2.StatusDescription)"
-			add-content -path "/var/log/web_checks/script.log" -value "${today}: $x StatusCode: $($response2.StatusCode) and StatusDescription: $($response2.StatusDescription)"
-        }
-        else {
-            echo "${today}: $x StatusCode: $($response2.StatusCode) and StatusDescription: $($response2.StatusDescription)"
-            add-content -path "/var/log/web_checks/script.log" -value "${today}: $x StatusCode: $($response2.StatusCode) and StatusDescription: $($response2.StatusDescription)"
-            exit 1
-        }
-    }
-}
+    	for ($a = 1; $a -le 3; $a++) {
 		
+			$watch = [System.Diagnostics.Stopwatch]::StartNew()
+		
+			$response2 = invoke-webrequest -uri $x -TimeoutSec 10 -ErrorAction Stop 
+			$watch.Stop()
+			$time += $watch.Elapsed.TotalMilliseconds
+
+        	if ($response2.StatusCode -eq 200) {
+            	write-output "${today}: $x StatusCode: $($response2.StatusCode) and StatusDescription: $($response2.StatusDescription)"
+				add-content -path $logfile -value "${today}: $x StatusCode: $($response2.StatusCode) and StatusDescription: $($response2.StatusDescription)"
+        	}
+       		else {
+            	write-output "${today}: $x StatusCode: $($response2.StatusCode) and StatusDescription: $($response2.StatusDescription)"
+            	add-content -path $logfile -value "${today}: $x StatusCode: $($response2.StatusCode) and StatusDescription: $($response2.StatusDescription)"
+        	exit 1
+       		}
+    	}
+    }
+
+	#Write-Host $time 
+	Caltime -time $time
+
 }
 
-
-WebRequest
+ WebRequest
 
 
